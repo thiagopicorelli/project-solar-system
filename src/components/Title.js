0b5337da1b9139944e86c6d8fc48057f86c6d477
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:03:06 -0300

    Adiciona componente Title

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return (<h2>{ headline }</h2>);
+  }
+}
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+Title.defaultProps = {
+  headline: '',
+};
+export default Title;
