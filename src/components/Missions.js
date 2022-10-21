73a5979004d64698e55aedbf9eede4a485def9d0
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:29:00 -0300

    Cria componente de missões e adiciona o título

---
+++ src/components/Missions.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import Title from './Title';
+
+class Missions extends Component {
+  render() {
+    return (
+    <div data-testid="missions">
+      <Title headline="Missões" />
+    </div>);
+  }
+}
+export default Missions;
