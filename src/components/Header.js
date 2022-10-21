95851f41e7623513acb29ae6e276c5188417b2c8
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:12:53 -0300

    Adiciona tag header ao componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return (<h1>Sistema Solar</h1>);
+    return (<header><h1>Sistema Solar</h1></header>);
   }
 }
 export default Header;
