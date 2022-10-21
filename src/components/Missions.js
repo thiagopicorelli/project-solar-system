0e149cebd53b32bc851c2b60f3d8b0dd2736d5a2
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:37:37 -0300

    Cria componente MissionCard e corrige lint no Missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -4,9 +4,10 @@ import Title from './Title';
 class Missions extends Component {
   render() {
     return (
-    <div data-testid="missions">
-      <Title headline="Missões" />
-    </div>);
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
   }
 }
 export default Missions;
