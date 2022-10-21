5e1da9d3cee229050ed0a0c1d4355bf1889a3194
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:26:08 -0300

    Adiciona a lista de planetas do sistema solar

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,23 @@
 import { Component } from 'react';
+import planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
-    return (<div data-testid="solar-system"><Title headline="Planetas" /></div>);
+    const listPlanets = planets.map((planet, index) => (
+      <PlanetCard
+        key={ index }
+        planetName={ planet.name }
+        planetImage={ planet.image }
+      />
+    ));
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        { listPlanets }
+      </div>
+    );
   }
 }
 export default SolarSystem;
