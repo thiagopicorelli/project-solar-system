bff8302905b1d160a94f60e5c71d5a3945c56054
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 14:57:42 -0300

    Cria componente SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (<div data-testid="solar-system">Sistema Solar</div>);
+  }
+}
+export default SolarSystem;
