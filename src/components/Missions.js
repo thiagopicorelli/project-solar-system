de2cd95eab2de5fb0d8598359a8642abe137e5d0
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-21 15:40:41 -0300

    Cria lista de missões

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,23 @@
 import { Component } from 'react';
+import missions from '../data/missions';
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
+    const listMission = missions.map((mission, index) => (
+      <MissionCard
+        key={ index }
+        name={ mission.name }
+        year={ mission.year }
+        country={ mission.country }
+        destination={ mission.destination }
+      />
+    ));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { listMission }
       </div>
     );
   }
